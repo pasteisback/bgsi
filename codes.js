6866d1f95fe1e001125f4c93a9a6433e9255b20e
Cactus <roccoagiacomino@gmail.com>
2025-04-20 17:44:48 -0500

    Delete codes.js

--- codes.js
+++
@@ -1,5 +0,0 @@
-const codes = [
-    { code: "BGSI", reward: "500 Coins", source: "Game" },
-    { code: "TWITTERCODE", reward: "100 Gems", source: "Twitter" },
-    { code: "DISCORDDROP", reward: "Exclusive Pet", source: "Discord" }
-];
