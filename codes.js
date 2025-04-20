5c33786b9bf3b6769924bb3b8b038fec677f61f6
Cactus <roccoagiacomino@gmail.com>
2025-04-20 12:52:55 -0500

    Create codes.js

---
+++ codes.js
@@ -0,0 +1,5 @@
+const codes = [
+    { code: "BGSI", reward: "500 Coins", source: "Game" },
+    { code: "TWITTERCODE", reward: "100 Gems", source: "Twitter" },
+    { code: "DISCORDDROP", reward: "Exclusive Pet", source: "Discord" }
+];
